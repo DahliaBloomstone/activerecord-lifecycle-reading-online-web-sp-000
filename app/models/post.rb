d347b7d80ecce0cd1709920705ddd5d79ed6ac20
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-04 00:01:28 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,11 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case
 
-  private
+  # New Code!!
+    before_save :make_title_case
 
+
+  private
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
       errors.add(:title, "Title must be in title case")
@@ -14,7 +17,7 @@ class Post < ActiveRecord::Base
 # make sure validation always passes vefore every save
 # rails to run our title-case algorithm on the title of the Post
 # to make sure all of our Posts have the correctly formatted title, we are going to run make_title_case during the first of the available lifecycle points
-# before_save 
+# before_save
   def make_title_case
     self.title = self.title.titlecase
   end
